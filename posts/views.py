3e41b8ca024175a180cc702fdfdfac075e8e7e02
piet <pprotassow@posteo.de>
2024-09-02 13:08:45 +0200

    TemplateView vs. ListView

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,17 @@
+from django.views.generic import TemplateView
 from django.views.generic import ListView
 from .models import Post
 
 class HomePageView(ListView):
-    model = Post
-    template_name = "home.html"
+     model = Post
+     template_name = "home.html"
+
+
+# class HomePageView(TemplateView):
+#     template_name = "home.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['post_list'] = Post.objects.all()
+#         return context
 
