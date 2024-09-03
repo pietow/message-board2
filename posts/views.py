d3641a2d4198ee4b146abaee87a6140e46099a44
piet <pprotassow@posteo.de>
2024-09-03 11:53:46 +0200

    added tests

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+from .models import Post
 
-# Create your views here.
+
+class HomePageView(ListView):
+    template_name = "home.html"
+    model = Post
+
+# class HomePageView(TemplateView):
+#     template_name = "home.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['post_list'] = list(Post.objects.all())
+#         # context['post_list'].append('Bla2') 
+#         return context
+
